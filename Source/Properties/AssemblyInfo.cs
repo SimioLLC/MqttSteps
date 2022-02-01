8785ff617a852c784c08e3c3d9784c8fc826d0cd
Dan Houck <dan.houck@batpox.com>
2022-02-01 05:47:59 -0500

    Target 64bit Simio (Version 14.231). Updated packages.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Simio LLC")]
 [assembly: AssemblyProduct("Mqttteps")]
-[assembly: AssemblyCopyright("Copyright © Simio LLC 2020")]
+[assembly: AssemblyCopyright("Copyright © Simio LLC 2020-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.10.21.0")]
-[assembly: AssemblyFileVersion("20.10.21.0")]
+[assembly: AssemblyVersion("22.01.31.0")]
+[assembly: AssemblyFileVersion("22.01.31.0")]
